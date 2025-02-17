1e0c308aa2e3bf69e64dffe06f7cbfde9c607761
ManuelMolino <mmolmil158@g.educaand.es>
2025-02-17 12:55:21 +0100

    Ejercicios propuesto lectura de ficheros usando stream

---
+++ src/ficheros/Main.java
@@ -0,0 +1,11 @@
+package ficheros;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        //usando Scanner leemos el fichero datos.csv y llenamos la lista
+    }
+}
---
+++ src/ficheros/Person.java
@@ -0,0 +1,4 @@
+package ficheros;
+
+public record Person(int id, String firstName, String lastName, String email) {
+}
